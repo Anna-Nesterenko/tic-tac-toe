bdd6c30c7802383c30e27d6ab36e5930bb7c738c
AnnaNesterenko <nura_arh@ukr.net>
2022-06-07 15:50:51 +0300

    apgraideAll

--- script.js
+++ script.js
@@ -1,73 +1,114 @@
-const area = document.getElementById('area');
-let move = 0;
-let result = ' ';
-const contentWrapper = document.getElementById('content');
-const modalResult = document.getElementById('modal-result-wrapper');
-const overlay = document.getElementById('overlay');
-const bntClose = document.getElementById('btn-close');
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
 
-area.addEventListener('click', e => {
-  if (e.target.innerHTML) {
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let isGameActive = true;
+
+const PLAYERX_WON = 'PLAYERX_WON';
+const PLAYERO_WON = 'PLAYERO_WON';
+const TIE = 'TIE';
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i += 1) {
+    const winCondition = winningConditions[i];
+    const a = board[winCondition[0]];
+    const b = board[winCondition[1]];
+    const c = board[winCondition[2]];
+
+    if (a === '' || b === '' || c === '') {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+    isGameActive = false;
     return;
   }
-  if (e.target.className === 'box') {
-    move % 2 === 0 ? (e.target.innerHTML = 'X') : (e.target.innerHTML = '0');
-    move++;
-    check();
+
+  if (!board.includes('')) announce(TIE);
+}
+
+const announce = type => {
+  switch (type) {
+    case PLAYERO_WON:
+      announcer.innerHTML = 'Гравець <span class="playerO"> O</span> виграв';
+      break;
+    case PLAYERX_WON:
+      announcer.innerHTML = 'Гравець <span class="playerX"> X</span> виграв';
+      break;
+    case TIE:
+      announcer.innerText = 'Нічия';
   }
+  announcer.classList.remove('hide');
+};
+
+const isValidAction = tile => {
+  if (tile.innerText === 'X' || tile.innerText === 'O') {
+    return false;
+  }
+
+  return true;
+};
+
+const updateBoard = index => {
+  board[index] = currentPlayer;
+};
+
+const changePlayer = () => {
+  playerDisplay.classList.remove(`player${currentPlayer}`);
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  playerDisplay.innerText = currentPlayer;
+  playerDisplay.classList.add(`player${currentPlayer}`);
+};
+
+const userAction = (tile, index) => {
+  if (isValidAction(tile) && isGameActive) {
+    tile.innerText = currentPlayer;
+    tile.classList.add(`player${currentPlayer}`);
+    updateBoard(index);
+    handleResultValidation();
+    changePlayer();
+  }
+};
+
+const resetBoard = () => {
+  board = ['', '', '', '', '', '', '', '', ''];
+  isGameActive = true;
+  announcer.classList.add('hide');
+
+  if (currentPlayer === 'O') {
+    changePlayer();
+  }
+
+  tiles.forEach(tile => {
+    tile.innerText = '';
+    tile.classList.remove('playerX');
+    tile.classList.remove('playerO');
+  });
+};
+
+tiles.forEach((tile, index) => {
+  tile.addEventListener('click', () => userAction(tile, index));
 });
 
-const check = () => {
-  const boxes = document.getElementsByClassName('box');
-
-  const arr = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (
-      boxes[arr[i][0]].innerHTML === 'X' &&
-      boxes[arr[i][1]].innerHTML === 'X' &&
-      boxes[arr[i][2]].innerHTML === 'X'
-    ) {
-      result = 'хрестики';
-      prepareResult(result);
-    } else if (
-      boxes[arr[i][0]].innerHTML === '0' &&
-      boxes[arr[i][1]].innerHTML === '0' &&
-      boxes[arr[i][2]].innerHTML === '0'
-    ) {
-      result = 'нулики';
-      prepareResult(result);
-    }
-    if (move === 9 && !arr) {
-      result = 'Нічия';
-      prepareResult2(result);
-    }
-  }
-};
-
-const prepareResult = winner => {
-  contentWrapper.innerHTML = `Виграли ${winner}!`;
-  modalResult.style.display = 'block';
-};
-
-const prepareResult2 = winner => {
-  contentWrapper.innerHTML = `${winner}!`;
-  modalResult.style.display = 'block';
-};
-
-const closeModal = () => {
-  modalResult.style.display = 'none';
-  location.reload();
-};
-
-overlay.addEventListener('click', closeModal);
-bntClose.addEventListener('click', closeModal);
+resetButton.addEventListener('click', resetBoard);
